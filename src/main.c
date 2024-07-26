66a23bfa9208f5a1d64a8528c7d9b3dccfb264ac
Lennart Rittel <33598633+lrittel@users.noreply.github.com>
2024-07-26 21:35:40 +0200

    Switch from astyle to clang-format

--- src/main.c
+++ src/main.c
@@ -1,8 +1,8 @@
-#include <stdbool.h>
 #include <assert.h>
+#include <stdbool.h>
 
-#include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
+#include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 
 #include "version.h"
@@ -15,24 +15,24 @@ static LOG_MODULE_REGISTER(main);
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
 int main(void) {
-    int result;
+  int result;
 
-    if (!device_is_ready(led.port)) {
-        return -ENODEV;
-    }
+  if (!device_is_ready(led.port)) {
+    return -ENODEV;
+  }
 
-    LOG_INF("Application version: %s", APPLICATION_VERSION);
+  LOG_INF("Application version: %s", APPLICATION_VERSION);
 
-    result = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+  result = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+  if (result < 0) {
+    return result;
+  }
+
+  while (true) {
+    result = gpio_pin_toggle_dt(&led);
     if (result < 0) {
-        return result;
+      return result;
     }
-
-    while (true) {
-        result = gpio_pin_toggle_dt(&led);
-        if (result < 0) {
-            return result;
-        }
-        k_msleep(500);
-    }
+    k_msleep(500);
+  }
 }
